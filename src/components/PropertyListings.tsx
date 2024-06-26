6254396f2af42105b1432fe1b04a575ee2b98cc9
iibrahim70 <iibrahiim.dev@gmail.com>
2024-06-26 21:57:12 +0600

    :hammer: update property listings

--- src/components/PropertyListings.tsx
+++ src/components/PropertyListings.tsx
@@ -1,50 +1,18 @@
-import propertyImageOne from "@/assets/images/property-img-1.png";
-import propertyImageTwo from "@/assets/images/property-img-2.png";
-import propertyImageThree from "@/assets/images/property-img-3.png";
-import propertyImageFour from "@/assets/images/property-img-4.png";
-import propertyImageFive from "@/assets/images/property-img-5.png";
-import propertyImageSix from "@/assets/images/property-img-6.png";
+import data from "@/assets/data/propertyListings.json";
+import { cn } from "@/lib/utils";
 import playerLogo from "@/assets/icons/video-player.png";
 import { Button } from "./ui/button";
 import { IoSearch } from "react-icons/io5";
-import { cn } from "@/lib/utils";
 
 const PropertyListings = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Modern Family Homes",
-      subTitle: "Spacious and Comfortable Living",
-      description:
-        "Discover modern family homes designed for comfort and convenience. These homes offer spacious living areas, contemporary kitchens, and plenty of outdoor space for your family to enjoy.",
-      images: [propertyImageOne, propertyImageTwo],
-    },
-    {
-      id: 2,
-      title: "Luxury Apartments",
-      subTitle: "Elegant and High-End Living",
-      description:
-        "Experience the height of luxury in our premium apartments. These residences feature top-of-the-line amenities, breathtaking views, and impeccable design details to ensure a sophisticated lifestyle.",
-      images: [propertyImageThree, propertyImageFour],
-    },
-    {
-      id: 3,
-      title: "Cozy Cottages",
-      subTitle: "Charming and Quaint Getaways",
-      description:
-        "Escape to a cozy cottage, perfect for a peaceful retreat. These charming homes are nestled in scenic locations and offer a warm and inviting atmosphere, ideal for relaxation and rejuvenation.",
-      images: [propertyImageFive, propertyImageSix],
-    },
-  ];
-
   return (
-    <section className="section-wrapper space-y-10 mb-20">
+    <section className="section-wrapper py-20">
       {data?.map((item, index) => (
         <div
           key={item?.id}
           className={cn(
-            "flex max-md:flex-col items-center justify-between gap-20 relative py-20",
-            index % 2 !== 0 && "flex-row-reverse"
+            "relative flex max-md:flex-col items-center justify-between gap-20",
+            index % 2 !== 0 ? "flex-row-reverse pt-40 pb-20" : "pb-20"
           )}
         >
           {/* left side */}
